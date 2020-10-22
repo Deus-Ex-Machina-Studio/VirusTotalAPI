32079801a409d494f8178b2973fac27f831d31d2
shogo-makishima <48924544+shogo-makishima@users.noreply.github.com>
2020-10-22 23:57:31 +0300

    Initial commit

---
+++ VirusTotalAPI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program {
+
+    static void Main(string[] args) {
+        VirusTotal.API.APIKEY = "<ВАШ APIKEY>";
+
+        for (int i = 0; i < 25; i++) {
+            VirusTotal.API.ScanFile("E:/PIL20.py");
+            VirusTotal.API.ReportFile();
+            Console.WriteLine($"SCANS: {VirusTotal.API.scans.Count}, LAST_SCAN: {VirusTotal.API.scans[0].date}");
+            System.Threading.Thread.Sleep(20000);
+        }
+
+        Console.ReadLine();
+    }
+}
+
---
+++ VirusTotalAPI/VirusTotalAPI.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Net.Http;
+using System.IO;
+using Newtonsoft.Json.Serialization;
+
+namespace VirusTotal {
+    /// <summary>
+    /// Модуль API
+    /// </summary>
+    class API {
+        /// <summary>
+        /// Клиент для загрузки файла на сервер VirusTotal
+        /// </summary>
+        static WebClient webClient = new WebClient();
+
+        /// <summary>
+        /// URL
+        /// </summary>
+        const string URL = "https://www.virustotal.com/vtapi/v2";
+
+        /// <summary>
+        /// Ключ api
+        /// </summary>
+        public static string APIKEY = "";
+
+        /// <summary>
+        /// Файлы, отправленные на сканирование
+        /// </summary>
+        public static RScan lastScan = null;
+
+        /// <summary>
+        /// Максимальное кол-во записей в истории
+        /// </summary>
+        public static int maxScansList = 15;
+
+        /// <summary>
+        /// Результаты сканирования
+        /// </summary>
+        public static List<ScanResult> scans = new List<ScanResult> { };
+
+        /// <summary>
+        /// Отправить файл на проверку
+        /// </summary>
+        /// <param name="filePath"> - путь к файлу. </param>
+        public static void ScanFile(string filePath) {
+            byte[] p_request = webClient.UploadFile($"{URL}/file/scan?apikey={APIKEY}", filePath);
+            string s_json = Encoding.ASCII.GetString(p_request);
+            lastScan = JsonSerializer.Deserialize<RScan>(s_json);
+        }
+
+        /// <summary>
+        /// Получить результат сканирования файла
+        /// </summary>
+        public static void ReportFile() {
+            try {
+                string s_json = GET($"{URL}/file/report?apikey={APIKEY}&resource={lastScan.resource}");
+                ScanResult scan = JsonSerializer.Deserialize<ScanResult>(s_json);
+                scan.date = DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss");
+                scans.Insert(0, scan);
+                if (scans.Count >= maxScansList) scans = SeparateList<ScanResult>(scans, 0, maxScansList);
+                lastScan = null;
+            } catch (Exception exception) {
+                Console.WriteLine(exception);
+            }
+        }
+
+        /// <summary>
+        /// Обрезка списка в диапозоне [min, max]
+        /// </summary>
+        /// <typeparam name="T"> - тип.</typeparam>
+        /// <param name="list"> - исходный массив.</param>
+        /// <param name="min"> - минимальный индекс.</param>
+        /// <param name="max"> - максимальный индекс. </param>
+        /// <returns></returns>
+        public static List<T> SeparateList<T>(List<T> list , int min, int max) {
+            List<T> t_list = new List<T> { };
+
+            int i_min = (min < 0 && min > list.Count) ? 0 : min;
+            int i_max = (max < list.Count) ? max : list.Count;
+            
+            for (int i = i_min; i < i_max; i++) {
+                t_list.Add(list[i]);
+            }
+
+            return t_list;
+        }
+
+        /// <summary>
+        /// Простой метод GET
+        /// </summary>
+        /// <param name="url"> - url.</param>
+        /// <returns></returns>
+        public static string GET(string url) {
+            WebRequest request = WebRequest.Create(url);
+            request.Credentials = CredentialCache.DefaultCredentials;
+
+            WebResponse response = request.GetResponse();
+
+            string t_string = "";
+
+            using (Stream dataStream = response.GetResponseStream()) {
+                StreamReader reader = new StreamReader(dataStream);
+                t_string += reader.ReadToEnd();
+            }
+
+            response.Close();
+
+            return t_string;
+        }
+    }
+
+    /// <summary>
+    /// Данные после запуска сканирования
+    /// </summary>
+    [System.Serializable]
+    class RScan {
+        public string scan_id { set; get; }
+        public string sha1 { set; get; }
+        public string resource { set; get; }
+        public string sha256 { set; get; }
+        public string permalink { set; get; }
+        public string md5 { set; get; }
+        public string verbose_msg { set; get; }
+        public int response_code { set; get; }
+    }
+
+    /// <summary>
+    /// Отчёт по базе сканирования
+    /// </summary>
+    [System.Serializable]
+    class Result {
+        public string name { set; get; }
+        public bool detected { set; get; }
+        public string version { set; get; }
+        public string result { set; get; }
+        public string update { set; get; }
+    }
+
+    /// <summary>
+    /// Результат сканирования
+    /// </summary>
+    [System.Serializable]
+    class ScanResult : RScan {
+        public Dictionary<string, Result> scans { set; get; }
+        public int total { set; get; }
+        public int positives { set; get; }
+        public string date { set; get; }
+    }
+}
